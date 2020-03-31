32a1be93edafb9d535abaa3ef137b6d80e2ecb85
bbatta0328 <50049259+bbatta0328@users.noreply.github.com>
2020-03-31 21:24:09 +0900

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,10 +1,18 @@
 import discord
+import asyncio
+import os
+from discord.ext import commands
+from discord.ext.commands import bot
 
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print('Kardien Start')
-
+    
+@bot.command()
+async def ip(ctx):
+    await ctx.send('Kardien.kro.kr')    
 
 access_token = os.environ["BOT_TOKEN"]    
 bot.run(access_token)
